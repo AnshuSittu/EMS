6cb1ef0958289a0be19dd45ee11f54f90cd3f02c
Anshu Raj Singh <72456550+AnshuSittu@users.noreply.github.com>
2025-09-24 23:57:00 +0530

    Inital commit

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+     console.log("Email", email); 
+     console.log("Password Is", password); 
+
+     setEmail("")
+     setPassword("")
+    }
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
+        <div className="w-full max-w-sm bg-white rounded-2xl shadow-lg p-8">
+          <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">
+            Login
+          </h2>
+          <form onSubmit={(e) => {
+            submitHandler(e)
+          }} className="flex flex-col space-y-4">
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+               setEmail(e.target.value)
+              }}
+              required
+              placeholder="Enter your email"
+              className="w-full px-4 py-2 border border-gray-300 rounded-xl 
+                         focus:ring-2 focus:ring-blue-500 focus:outline-none 
+                         focus:border-blue-500 shadow-sm transition"
+
+            />
+
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value)
+              }}
+              
+              required
+              placeholder="Enter your password"
+              className="w-full px-4 py-2 border border-gray-300 rounded-xl 
+                         focus:ring-2 focus:ring-blue-500 focus:outline-none 
+                         focus:border-blue-500 shadow-sm transition"
+            />
+
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white py-2 rounded-xl 
+                         hover:bg-blue-700 transition font-medium shadow-md"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
